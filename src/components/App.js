c743a2d638ba335416a9e5fdbb18e1558efedc02
bsp3725k <bhavanisivaprasad39@gmail.com>
2025-01-23 13:40:50 +0530

    File updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
-
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  // Function to handle the button click for Option 1
+  const handleOption1 = () => {
+    setSelectedOption('Option 1');
+  };
+
+  // Function to handle the button click for Option 2
+  const handleOption2 = () => {
+    setSelectedOption('Option 2');
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div style={{backgroundColor:'#37eb34', padding:'10px', alignItems:'center' }}>
+       <h1>Parent Component</h1>
+       <div style={{backgroundColor:'orange', padding:'0px'}}>
+        <h1 style={{padding:'5px'}}>Child Component 1</h1>
+        <button style={{width:'150px',height:'30px', margin:'5px', borderRadius:'10px'}} onClick={handleOption1} >Option 1</button>
+       </div>
+       <div style={{backgroundColor:'yellow'}}>
+        <h1 style={{padding:'5px'}}>Child Component 2</h1>
+        <button style={{width:'150px',height:'30px', margin:'5px', borderRadius:'10px'}} onClick={handleOption2} >Option 2</button>
+       </div>
+       <p>Selected Option: {selectedOption} </p>
     </div>
   )
 }
